212690490a8b7508505f05a6bbab623c5b402497
B-Panther <150675493+B-Panther@users.noreply.github.com>
2023-11-13 16:33:25 +0500

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,38 @@
+import requests
+
+def run_script(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            exec(response.text)
+        else:
+            print(f"Failed to fetch script from {url}. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def main():
+    print("Select a script to run:")
+    print("1. ANDROID FBAN")
+    print("2. IPHONE FBIOS")
+    print("3. ANDROID DALVIK")
+    print("4. ANDROID DAVIK ORCA")
+    print("5. ANDROID ORCA")
+
+    try:
+        choice = int(input("Enter your choice (1-5): "))
+        if 1 <= choice <= 5:
+            script_urls = [
+                "https://raw.githubusercontent.com/your_username/repository/script1.py",
+                "https://raw.githubusercontent.com/your_username/repository/script2.py",
+                "https://raw.githubusercontent.com/your_username/repository/script3.py",
+                "https://raw.githubusercontent.com/your_username/repository/script4.py",
+                "https://raw.githubusercontent.com/your_username/repository/script5.py",
+            ]
+            run_script(script_urls[choice - 1])
+        else:
+            print("Invalid choice. Please enter a number between 1 and 5.")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+
+if __name__ == "__main__":
+    main()
